b5e492cc47b39655d9bcc0d331a81f5270e4bc60
stianste <stianste@stud.ntnu.no>
2018-01-26 21:28:09 +0100

    Make trec_eval path a variable

--- run_evaluations.sh
+++ run_evaluations.sh
@@ -1,42 +1,44 @@
+relative_trec_eval_path=~/Downloads/trec_eval/trec_eval
+
 # Run tfidf evaulation
 filename="tfidf"
 output_filename="$filename"_results
-~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$filename".run | grep "\sall\s" > ./lexical_results/"$output_filename"_testset.txt
-~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$filename".run | grep "\sall\s" > ./lexical_results/"$output_filename"_validation.txt
+$relative_trec_eval_path -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$filename".run | grep "\sall\s" > ./lexical_results/"$output_filename"_testset.txt
+$relative_trec_eval_path -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$filename".run | grep "\sall\s" > ./lexical_results/"$output_filename"_validation.txt
 
 # BM25
 filename="BM25"
 output_filename="$filename"_results
-~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$filename".run | grep "\sall\s" > ./lexical_results/"$output_filename"_testset.txt
-~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$filename".run | grep "\sall\s" > ./lexical_results/"$output_filename"_validation.txt
+$relative_trec_eval_path -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$filename".run | grep "\sall\s" > ./lexical_results/"$output_filename"_testset.txt
+$relative_trec_eval_path -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$filename".run | grep "\sall\s" > ./lexical_results/"$output_filename"_validation.txt
 
 run_parameters_between_1 () {
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_0.1.run | grep "\sall\s" > ./lexical_results/"$2"_0.1_testset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_0.3.run | grep "\sall\s" > ./lexical_results/"$2"_0.3_testset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_0.5.run | grep "\sall\s" > ./lexical_results/"$2"_0.5_testset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_0.7.run | grep "\sall\s" > ./lexical_results/"$2"_0.7_testset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_0.9.run | grep "\sall\s" > ./lexical_results/"$2"_0.9_testset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_0.1.run | grep "\sall\s" > ./lexical_results/"$2"_0.1_testset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_0.3.run | grep "\sall\s" > ./lexical_results/"$2"_0.3_testset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_0.5.run | grep "\sall\s" > ./lexical_results/"$2"_0.5_testset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_0.7.run | grep "\sall\s" > ./lexical_results/"$2"_0.7_testset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_0.9.run | grep "\sall\s" > ./lexical_results/"$2"_0.9_testset.txt
 
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_0.1.run | grep "\sall\s" > ./lexical_results/"$2"_0.1_validationset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_0.3.run | grep "\sall\s" > ./lexical_results/"$2"_0.3_validationset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_0.5.run | grep "\sall\s" > ./lexical_results/"$2"_0.5_validationset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_0.7.run | grep "\sall\s" > ./lexical_results/"$2"_0.7_validationset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_0.9.run | grep "\sall\s" > ./lexical_results/"$2"_0.9_validationset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_0.1.run | grep "\sall\s" > ./lexical_results/"$2"_0.1_validationset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_0.3.run | grep "\sall\s" > ./lexical_results/"$2"_0.3_validationset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_0.5.run | grep "\sall\s" > ./lexical_results/"$2"_0.5_validationset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_0.7.run | grep "\sall\s" > ./lexical_results/"$2"_0.7_validationset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_0.9.run | grep "\sall\s" > ./lexical_results/"$2"_0.9_validationset.txt
 }
 
 run_dirichlet () {
   # Old version: ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$filename"_500.run | grep "\sall\s" > ./lexical_results/"$output_filename"_500_testset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_500.run | grep "\sall\s" > ./lexical_results/"$2"_500_testset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_1000.run | grep "\sall\s" > ./lexical_results/"$2"_1000_testset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_1500.run | grep "\sall\s" > ./lexical_results/"$2"_1500_testset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_500.run | grep "\sall\s" > ./lexical_results/"$2"_500_testset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_1000.run | grep "\sall\s" > ./lexical_results/"$2"_1000_testset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_test ./lexical_results/"$1"_1500.run | grep "\sall\s" > ./lexical_results/"$2"_1500_testset.txt
 
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_500.run | grep "\sall\s" > ./lexical_results/"$2"_500_validationset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_1000.run | grep "\sall\s" > ./lexical_results/"$2"_1000_validationset.txt
-  ~/Downloads/trec_eval/trec_eval -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_1500.run | grep "\sall\s" > ./lexical_results/"$2"_1500_validationset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_500.run | grep "\sall\s" > ./lexical_results/"$2"_500_validationset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_1000.run | grep "\sall\s" > ./lexical_results/"$2"_1000_validationset.txt
+  $relative_trec_eval_path -m all_trec -q ap_88_89/qrel_validation ./lexical_results/"$1"_1500.run | grep "\sall\s" > ./lexical_results/"$2"_1500_validationset.txt
 }
 
 
-# Jelinek-Mercer
+# # Jelinek-Mercer
 filename="jelinek_mercer"
 output_filename="$filename"_results
 
