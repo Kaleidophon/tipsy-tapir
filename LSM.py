7fc1268da10e1144e538daa47feaebbd4251efb0
Santhosh Kumar Rajamanickam <santhosh.rajamanickam@student.uva.nl>
2018-01-28 01:21:01 +0100

    Add LSI and LDA model creation

--- LSM.py
+++ LSM.py
@@ -1,16 +1,21 @@
-import gensim
 import pyndri
+import pyndri.compat
+
+import gensim
 from gensim import corpora
 from gensim import models
 from gensim.models import lsimodel
-from gensim.models import tfidfmodel
+from gensim.models import ldamodel
+
 import os
-import pyndri.compat
+
+# Hyperparameter to optimize
+NUM_TOPICS = 50
 
 index = pyndri.Index('index/')
 dictionary = pyndri.extract_dictionary(index)
 
-# 'Connect' class.
+# 'Connector' class.
 class CorpusConnector(object):
     def __init__(self, index):
         self.index = index
@@ -28,34 +33,10 @@ if not os.path.isfile('corpus.mm'):
 else:
     mm = corpora.MmCorpus('corpus.mm')
 
-# getting tfidf of the terms
-tfidf = tfidfmodel.TfidfModel(corpus = mm, id2word = dictionary, normalize = True)
+# LSI model
+lsi = lsimodel.LsiModel(corpus = mm, id2word = dictionary, num_topics = NUM_TOPICS)
+lsi.save('lsi.model')
 
-# transform tfidf to latent space
-lsi = lsimodel.LsiModel(corpus = tfidf[mm], id2word = dictionary, num_topics = 64)
-
-# Transformed_corpora
-tfidf_corpora = tfidf[mm]
-lsi_corpora = lsi[tfidf[mm]]
-
-
-for i, query in enumerate(queries.items()):
-    print("\nScoring query {} out of {} queries".format(i, len(queries)))
-    query_id, _ = query
-    query_term_ids = tokenized_queries[query_id]
-
-    query_scores = []
-
-    query_bow = dictionary.doc2bow(query_term_ids)
-    query_lsi = lsi[tfidf[query_bow]]
-
-    for n, document_id in enumerate(document_ids):
-        ext_doc_id, document_word_positions = index.document(document_id)
-        score = 0
-        for query_term_id in query_term_ids:
-            document_term_frequency = inverted_index[query_term_id][document_id]
-            score +=
-
-        query_scores.append((score, ext_doc_id))
-
-    data[query_id] = list(sorted(query_scores, reverse=True))[:max_objects_per_query]
+# LDA model
+lda = ldamodel.LdaModel(corpus = mm, num_topics = NUM_TOPICS, id2word=dictionary)
+lda.save('lda.model')
